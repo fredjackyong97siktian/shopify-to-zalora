000ed10d3042a768a4aae0ffa175c93fd0fa9685
SikTian Yong <siktianyong97@gmail.com>
2024-02-29 02:19:47 +0800

    server

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import uvicorn
-from main import main
+from main import app
 
 if __name__ == "__main__":
-    uvicorn.run(main, host="0.0.0.0", port=8181)
+    uvicorn.run(app, host="0.0.0.0", port=8181)
 
 
